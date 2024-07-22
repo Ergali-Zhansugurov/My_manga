9ef6c514a038b4c4ea169824c4add05deb89b195
Ergali-Zhansugurov <ergali.zhansugurov@mail.ru>
2024-07-22 20:47:45 +0500

    Add files via upload

---
+++ Tables.sql
@@ -0,0 +1,10 @@
+create table categories(
+    id SERIAL,
+    name  varchar(255),
+)
+Create table Titles (
+    id serial INTEGER ;
+    name varchar (200)
+    chapters int
+    Category varchar(255)
+)
